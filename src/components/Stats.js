f137aaa32cc539c044da2b5c24ce831ec596eb3f
Ryan Albrecht <ryanalbrecht@pinterest.com>
2017-04-05 15:48:15 -0700

    Hide the chunk graph behind a click once a chunk has been picked.

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -9,6 +9,7 @@ import ChunkParentList from './stats/ChunkParentList';
 import getExtendedModulesById from '../stats/getExtendedModulesById';
 import ModuleTable from './stats/ModuleTable';
 import React, { Component } from 'react';
+import ShowablePanel from './ShowablePanel';
 
 type Props = {
   stats: {
@@ -30,19 +31,24 @@ export default class Stats extends Component<void, Props, State> {
     return (
       <div>
         <h3>Loaded: {this.props.stats.file}</h3>
-        <fieldset>
-          <h4>Chunk Graph</h4>
-          <ChunkGraph
-            stats={this.props.stats.raw}
-            selectedChunkId={this.state.selectedChunkId}
-            onSelectChunkId={this.onSelectChunkId}
-          />
-        </fieldset>
-
         {this.state.selectedChunkId
           ? this.renderSelectedChunk(this.props.stats.raw, this.state.selectedChunkId)
-          : null}
+          : this.renderChunkSelector(this.props.stats.raw)
+        }
       </div>
+    );
+  }
+
+  renderChunkSelector(stats: RawStats) {
+    return (
+      <fieldset>
+        <h4>Chunk Graph</h4>
+        <ChunkGraph
+          stats={stats}
+          selectedChunkId={null}
+          onSelectChunkId={this.onSelectChunkId}
+        />
+      </fieldset>
     );
   }
 
@@ -55,15 +61,20 @@ export default class Stats extends Component<void, Props, State> {
 
     return (
       <fieldset>
-        <h4>Selected Chunk = {String(selectedChunkId)}</h4>
-
-        <fieldset>
-          <h4>Showing Modules inside Chunks</h4>
+        <ShowablePanel
+          key={'showing' + selectedChunkId}
+          trigger={'click'}
+          panel={<ChunkGraph
+            stats={stats}
+            selectedChunkId={selectedChunkId}
+            onSelectChunkId={this.onSelectChunkId}
+          />}>
+          <h4>Chunk Graph - Selected {String(selectedChunkId)}</h4>
           <ChunkParentList
             stats={stats}
             selectedChunkId={selectedChunkId}
           />
-        </fieldset>
+        </ShowablePanel>
 
         <fieldset>
           <h4>Module Table</h4>
