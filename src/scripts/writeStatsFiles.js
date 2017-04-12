29c6d01defcb5269e803cb9c934aa29e3cc64727
Ryan Albrecht <ryanalbrecht@pinterest.com>
2017-04-12 10:06:43 -0700

    Re-add the pull/write data files scripts
    accidentally removed, because /build conflicts with create-react-app's prod output folder!

---
+++ src/scripts/writeStatsFiles.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+
+const dataFolder = path.resolve(`${__dirname}/../../public/data/`);
+const indexFile = path.resolve(`${dataFolder}/index.json`);
+const webDataFolderPrefix = '/data/';
+
+console.log('Reading from', dataFolder);
+
+const files = fs.readdirSync(dataFolder)
+  .filter((file) => file !== 'index.json')
+  .filter((file) => file.endsWith('.json'))
+  .map((file) => webDataFolderPrefix + file);
+
+console.log('Found data files', files);
+
+fs.writeFileSync(indexFile, JSON.stringify({
+  files: files,
+}, null, '\t'));
+
+console.log('Recorded into ', indexFile);
