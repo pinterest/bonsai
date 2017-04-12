022d2b7a7a96c8909b1fdc65df69baef7945f656
Ryan Albrecht <ryanalbrecht@pinterest.com>
2017-04-11 21:18:32 -0700

    Revert <Unit> back to using 1000 as the base for easier human reading/comparing

--- src/components/Unit.js
+++ src/components/Unit.js
@@ -14,7 +14,7 @@ function formatBytes(bytes) {
   if (bytes === 0) {
     return '0 B';
   }
-  const k = 1024;
+  const k = 1000;
   const dm = 2;
   const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
   const i = Math.floor(Math.log(bytes) / Math.log(k));
