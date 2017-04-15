9abf292b27622290b24d258abb805f219380829a
Ryan Albrecht <ryanalbrecht@pinterest.com>
2017-04-15 15:20:33 -0700

    Let DropdownMenu close when clicking outside.

--- src/components/DropdownMenu.js
+++ src/components/DropdownMenu.js
@@ -25,16 +25,29 @@ export default class DropdownMenu extends Component<void, Props, State> {
     isOpen: false,
   };
 
+  _dropDownMenu: ?Node = null;
+  _flyout: ?Node = null;
+
+  componentDidMount() {
+    document.addEventListener('click', this.onDocumentClick);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.onDocumentClick);
+  }
+
   render() {
     return (
-      <div className={['btn-group', this.state.isOpen ? 'open' : ''].join(' ')}>
+      <div
+        className={['btn-group', this.state.isOpen ? 'open' : ''].join(' ')}
+        ref={(div) => this._dropDownMenu = div }>
         <button
           type="button"
           className="btn btn-default dropdown-toggle"
           data-toggle="dropdown"
           aria-haspopup="true"
           aria-expanded={this.state.isOpen}
-          onClick={() => this.setState({ isOpen: !this.state.isOpen })}>
+          onClick={this.onButtonClick}>
           {this.props.selectedItem.name}{' '}<span className="caret"></span>
         </button>
         {this.state.isOpen
@@ -46,12 +59,14 @@ export default class DropdownMenu extends Component<void, Props, State> {
 
   renderChildrenAsOptions() {
     return (
-      <ul className="dropdown-menu">
+      <ul
+        className="dropdown-menu"
+        ref={(ul) => this._flyout = ul }>
         {this.props.children.map((child) => (
           <li key={child.id}>
             <a
               href="#"
-              onClick={this.makeOnClick(child.id)}>
+              onClick={this.makeOnItemClick(child.id)}>
               {child.name}
             </a>
           </li>
@@ -60,7 +75,25 @@ export default class DropdownMenu extends Component<void, Props, State> {
     );
   }
 
-  makeOnClick(id: ID) {
+  onButtonClick = (event: MouseEvent) => {
+    if (this.state.isOpen) {
+      this.setState({isOpen: false});
+    } else {
+      this.setState({isOpen: true});
+    }
+  };
+
+  onDocumentClick = (event: MouseEvent) => {
+    if (this._dropDownMenu && this._dropDownMenu.contains(event.target)) {
+      // Ignore clicks coming from inside this component
+      return;
+    }
+    if (this.state.isOpen) {
+      this.setState({isOpen: false});
+    }
+  };
+
+  makeOnItemClick(id: ID) {
     return (event: SyntheticEvent) => {
       event.preventDefault();
       this.setState({
