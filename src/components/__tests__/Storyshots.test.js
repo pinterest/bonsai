bc2e5e33afd0347aee26ef80a033dc378e4588aa
Ryan Albrecht <ryanalbrecht@pinterest.com>
2017-04-16 15:29:09 -0700

    Note to come back and fixup storyshots into separate folders

--- src/components/__tests__/Storyshots.test.js
+++ src/components/__tests__/Storyshots.test.js
@@ -1,2 +1,4 @@
 import initStoryshots from 'storyshots';
+// TODO use storyKindRegex so we can put stories in related files/folders
+// See https://github.com/storybooks/storybook/pull/906
 initStoryshots();
