5476e91819b83e0d9bc9a1da4633eb079e85546d
Ryan Albrecht <ryanalbrecht@pinterest.com>
2017-04-16 15:36:52 -0700

    Move App storybook to it's own file.

---
+++ src/components/__stories__/App.stories.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { storiesOf, action } from '@kadira/storybook';
+
+import App from '../App';
+
+storiesOf('App', module)
+  .add('Default View', () => (
+    <App />
+  ));
--- src/stories/index.js
+++ src/stories/index.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
-import App from '../components/App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../index.css';
 
-storiesOf('App', module)
-  .add('Default View', () => (
-    <App />
-  ));
+import '../components/__stories__/App.stories';
