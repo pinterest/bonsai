484a4f58bd9cdd4885ac25b897896f2b6b1d5352
Ryan Albrecht <ryanalbrecht@pinterest.com>
2017-04-16 14:40:19 -0700

    Better config for global css in react-storybook

--- src/components/__tests__/Storyshots.test.js
+++ src/components/__tests__/Storyshots.test.js
@@ -1,2 +1,6 @@
 import initStoryshots from 'storyshots';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../../index.css';
+
 initStoryshots();
--- src/stories/index.js
+++ src/stories/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 import App from '../components/App';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../index.css';
+
 storiesOf('App', module)
   .add('Default View', () => (
     <App />
