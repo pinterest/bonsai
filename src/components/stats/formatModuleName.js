f78455f15f58f2d28fea6073e20b1f41a90e832e
Ryan Albrecht <ryanalbrecht@pinterest.com>
2017-04-17 01:56:54 -0700

    Use an ellipsis to shorten the query string of loaders when printing nice module names

--- src/components/stats/formatModuleName.js
+++ src/components/stats/formatModuleName.js
@@ -4,6 +4,8 @@
 
 import React from 'react';
 
+const ELLIPSIS = '\u2026';
+
 function joinWithBR(nodes, label, index) {
   return nodes.concat(label, <br key={index} />);
 }
@@ -14,12 +16,37 @@ export default function formatModuleName(name: string) {
     : [name];
 
   return names.map((name, i) => {
-    const bits = name.split('!');
-    const niceName = bits.pop();
-    bits.push('');
+    const parts = name.split('!');
+    const moduleName = parts.pop();
+
+    const loaders = parts.map((part) => {
+      const [loader, query] = part.split('?');
+      if (query) {
+        const shortQuery = query.length > 14
+          ? query.slice(0, 7) + ELLIPSIS + query.slice(-7)
+          : query;
+        return loader + '?' + shortQuery + '!'
+      }
+      return loader + '!';
+    });
+
+    const title = parts.length
+      ? parts.join('') + '!' + moduleName
+      : '';
+
     return [
-      <span key={'a' + i} style={{color: '#aaa'}}> {bits.join('!')}</span>,
-      <span key={'b' + i}>{niceName}</span>,
+      title
+        ? <span
+            key={'a' + i}
+            style={{color: '#aaa'}}
+            title={title}>{' '}{loaders.join('')}
+          </span>
+        : null,
+        <span
+          key={'b' + i}
+          title={title}>
+          {moduleName}
+        </span>,
     ];
   }).reduce(joinWithBR, []);
 }
