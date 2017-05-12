fd792ad7cdb9881e30fd16595df87b45ebccc78a
Ryan Albrecht <ryanalbrecht@pinterest.com>
2017-05-12 09:45:45 -0700

    Add aria label to external link component

--- src/components/stats/ExternalModuleLink.js
+++ src/components/stats/ExternalModuleLink.js
@@ -14,20 +14,15 @@ type Props = {
 };
 
 export default function ExternalModuleLink(props: Props) {
-  // https://phabricator.pinadmin.com/diffusion/P/browse/master/webapp/app/mobile/index.js$20
-  // https://phabricator.pinadmin.com/diffusion/P/browse/master/
-  // ../../../../../app/analytics/modules/SelectButtonWrapper/SelectButtonWrapper.js
-  // ../../../../../app/analytics/modules/SelectButtonWrapper/SelectButtonWrapper.js
-
-  // REACT_APP_FILE_URL_PREFIX=https://phabricator.pinadmin.com/diffusion/P/browse/master/webapp/a/b/c/d/e/
-
-  // https://phabricator.pinadmin.com/diffusion/P/browse/master/webapp/a/b/c/d/e/../../../../../app/analytics/modules/SelectButtonWrapper/SelectButtonWrapper.js
   if (props.prefix) {
     const file = props.module.name;
     const href = props.prefix + file;
     return (
       <Link href={href} newtab={true}>
-        <span className={getClassName('new-window')} />
+        <span
+          aria-label="Open in new window"
+          className={getClassName('new-window')}
+        />
       </Link>
     );
   } else {
