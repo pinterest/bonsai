87da628d3ed842e0e4da393fa76f8708ad5126c5
Ryan Albrecht <ryan@ryanalbrecht.ca>
2017-05-14 19:32:18 -0700

    Split Stats into a container, and renderer (poorly named).

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -4,11 +4,7 @@
 
 import type {ModuleID, RawStats} from '../types/Stats';
 
-import BlacklistTable from './stats/BlacklistTable';
-import ChunkBreadcrumb from './stats/ChunkBreadcrumb';
-import ChunkDropdown from './stats/ChunkDropdown';
-import fullModuleData from '../stats/fullModuleData';
-import ModuleTable from './stats/ModuleTable';
+import SelectedChunk from './stats/SelectedChunk';
 import React, { Component } from 'react';
 
 type Props = {
@@ -36,78 +32,16 @@ export default class Stats extends Component<void, Props, State> {
   }
 
   render() {
-    const {
-      moduleData,
-      extendedModules,
-    } = fullModuleData(
-      this.props.json,
-      this.state.selectedChunkId,
-      this.state.blacklistedModuleIds,
-    );
-
-    const blackList = moduleData && moduleData.removed.length
-      ? <div className="row">
-          <div className="col-sm-12">
-            <div className="panel panel-danger">
-              <div className="panel-heading">
-                {moduleData.removed.length} Modules Ignored
-              </div>
-              <BlacklistTable
-                blacklistedModulesIds={this.state.blacklistedModuleIds}
-                removedModules={moduleData.removed}
-                onIncludeModule={this.onIncludeModule}
-              />
-            </div>
-          </div>
-        </div>
-      : null;
-
-    const moduleTable = moduleData
-      ? <div className="row">
-          <div className="col-sm-12">
-            <div className="panel panel-primary">
-              <div className="panel-heading">
-                {moduleData.removed.length === 0
-                  ? 'All'
-                  : moduleData.included.length} Modules Included
-              </div>
-              <ModuleTable
-                extendedModules={extendedModules}
-                onRemoveModule={this.onRemoveModule}
-              />
-            </div>
-          </div>
-        </div>
-      : null;
-
     return (
-      <main className="container-fluid">
-        <div className="row">
-          <div className="col-sm-12">
-            <ChunkDropdown
-              stats={this.props.json}
-              selectedChunkId={this.state.selectedChunkId}
-              onSelectChunkId={this.onSelectChunkId}
-            />
-          </div>
-        </div>
-        {this.state.selectedChunkId
-          ? <div className="row">
-              <div className="col-sm-11 col-sm-push-1">
-                <ChunkBreadcrumb
-                  stats={this.props.json}
-                  selectedChunkId={this.state.selectedChunkId}
-                  totalModules={
-                    (moduleData ? moduleData.included.length : 0) +
-                    (moduleData ? moduleData.removed.length : 0)
-                  }
-                />
-              </div>
-            </div>
-          : null}
-        {blackList}
-        {moduleTable}
-      </main>
+      <SelectedChunk
+        json={this.props.json}
+        selectedChunkId={this.state.selectedChunkId}
+        blacklistedModuleIds={this.state.blacklistedModuleIds}
+
+        onSelectChunkId={this.onSelectChunkId}
+        onRemoveModule={this.onRemoveModule}
+        onIncludeModule={this.onIncludeModule}
+      />
     );
   }
 
---
+++ src/components/stats/SelectedChunk.js
@@ -0,0 +1,97 @@
+/*
+ * @flow
+ */
+
+import type {ModuleID, RawStats} from '../../types/Stats';
+
+import BlacklistTable from './BlacklistTable';
+import ChunkBreadcrumb from './ChunkBreadcrumb';
+import ChunkDropdown from './ChunkDropdown';
+import fullModuleData from '../../stats/fullModuleData';
+import ModuleTable from './ModuleTable';
+import React from 'react';
+
+type Props = {
+  json: RawStats,
+  selectedChunkId: ?number,
+  blacklistedModuleIds: Array<ModuleID>,
+  onSelectChunkId: (chunkId: string | number) => void,
+  onRemoveModule: (moduleID: ModuleID) => void,
+  onIncludeModule: (moduleID: ModuleID) => void,
+};
+
+export default function SelectedChunk(props: Props) {
+  const {
+    moduleData,
+    extendedModules,
+  } = fullModuleData(
+    props.json,
+    props.selectedChunkId,
+    props.blacklistedModuleIds,
+  );
+
+  const blackList = moduleData && moduleData.removed.length
+    ? <div className="row">
+        <div className="col-sm-12">
+          <div className="panel panel-danger">
+            <div className="panel-heading">
+              {moduleData.removed.length} Modules Ignored
+            </div>
+            <BlacklistTable
+              blacklistedModulesIds={props.blacklistedModuleIds}
+              removedModules={moduleData.removed}
+              onIncludeModule={props.onIncludeModule}
+            />
+          </div>
+        </div>
+      </div>
+    : null;
+
+  const moduleTable = moduleData
+    ? <div className="row">
+        <div className="col-sm-12">
+          <div className="panel panel-primary">
+            <div className="panel-heading">
+              {moduleData.removed.length === 0
+                ? 'All'
+                : moduleData.included.length} Modules Included
+            </div>
+            <ModuleTable
+              extendedModules={extendedModules}
+              onRemoveModule={props.onRemoveModule}
+            />
+          </div>
+        </div>
+      </div>
+    : null;
+
+  return (
+    <main className="container-fluid">
+      <div className="row">
+        <div className="col-sm-12">
+          <ChunkDropdown
+            stats={props.json}
+            selectedChunkId={props.selectedChunkId}
+            onSelectChunkId={props.onSelectChunkId}
+          />
+        </div>
+      </div>
+      {props.selectedChunkId
+        ? <div className="row">
+            <div className="col-sm-11 col-sm-push-1">
+              <ChunkBreadcrumb
+                stats={props.json}
+                selectedChunkId={props.selectedChunkId}
+                totalModules={
+                  (moduleData ? moduleData.included.length : 0) +
+                  (moduleData ? moduleData.removed.length : 0)
+                }
+              />
+            </div>
+          </div>
+        : null}
+      {blackList}
+      {moduleTable}
+    </main>
+  );
+}
