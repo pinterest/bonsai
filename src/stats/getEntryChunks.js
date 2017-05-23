2c907ed879a0e736c37fd9887b96d2f269d30859
Ryan Albrecht <ryanalbrecht@pinterest.com>
2017-05-23 11:45:42 -0700

    Show all entry points, named or not

--- src/stats/getEntryChunks.js
+++ src/stats/getEntryChunks.js
@@ -5,7 +5,8 @@
 import type {RawStats} from '../types/Stats';
 
 export default function getEntryChunks(stats: RawStats) {
-  return stats.chunks.filter(
-    chunk => chunk.names.length
-  );
+  return stats.chunks;
+  // .filter(
+  //   chunk => chunk.names.length
+  // );
 }
--- src/stats/getEntryHeirarchy.js
+++ src/stats/getEntryHeirarchy.js
@@ -6,8 +6,6 @@ import type {ChunkID, RawStats} from '../types/Stats';
 
 import getEntryChunks from './getEntryChunks';
 
-const localeChunkName = /(.*?)locale-[a-z]{2}(_[0-9A-Z]{2,3})?-(index|lite)(.*)/;
-
 export type Child = {
   id: ChunkID,
   ids: Array<ChunkID>,
@@ -16,39 +14,26 @@ export type Child = {
   children: Array<Child>,
 };
 
-function squashLocaleChunks(name: string) {
-  const match = name.match(localeChunkName);
-  if (match) {
-    return `${match[1]}local-.*-${match[3]}${match[4]}`;
-  }
-  return name;
+function getChunkName(chunk) {
+  return (
+    chunk.names.join(', ') ||
+    chunk.origins.map((origin) => origin.moduleName).join(', ')
+  );
 }
 
-function getChildrenForChunk(stats, parentChunk): Array<Child> {
-  // collect chunks with the same name here
-  const collectedChunksByName = {};
-
-  getEntryChunks(stats).filter(
-    (chunk) => chunk.parents.includes(parentChunk.id)
-  ).forEach((chunk) => {
-    const name = chunk.names.map(squashLocaleChunks).join(', ');
-
-    if (!collectedChunksByName[name]) {
-      collectedChunksByName[name] = {
-        id: chunk.id,
-        ids: [chunk.id],
-        name: name,
-        names: chunk.names,
-        children: getChildrenForChunk(stats, chunk),
-      };
-    } else {
-      collectedChunksByName[name].ids.push(chunk.id);
-    }
-  });
-
-  // $FlowFixMe: flow thinks `values` returns `Array<mixed>`
-  const children: Array<Child> = Object.values(collectedChunksByName);
-  return children;
+function getChildrenForChunk(stats, parentChunk, existingParents: Array<ChunkID>): Array<Child> {
+  return getEntryChunks(stats)
+    .filter((chunk) =>
+      chunk.parents.includes(parentChunk.id) &&
+      !existingParents.includes(chunk.id)
+    )
+    .map((chunk) => ({
+      id: chunk.id,
+      ids: [chunk.id],
+      name: getChunkName(chunk),
+      names: chunk.names,
+      children: getChildrenForChunk(stats, chunk, existingParents.concat(chunk.id)),
+    }));
 }
 
 export const ROOT_ID = Number.MIN_SAFE_INTEGER;
@@ -66,9 +51,9 @@ export default function getEntryHeirarchy(
       .map((chunk) => ({
         id: chunk.id,
         ids: [chunk.id],
-        name: chunk.names.map(squashLocaleChunks).join(', '),
+        name: getChunkName(chunk),
         names: chunk.names,
-        children: getChildrenForChunk(stats, chunk),
+        children: getChildrenForChunk(stats, chunk, [chunk.id]),
       })),
   };
 }
