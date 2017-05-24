aff68e8f83d5b77676d8b5dc020e81b9efd8d139
Ryan Albrecht <ryanalbrecht@pinterest.com>
2017-05-24 13:07:38 -0700

    Add some minimal tests for getEntryChunks

--- src/stats/__tests__/getEntryChunks.test.js
+++ src/stats/__tests__/getEntryChunks.test.js
@@ -2,15 +2,32 @@
  * @flow
  */
 
-import stats from './__fixtures__/stats.json';
-
+import {defaultChunk} from '../../__test_helpers__/defaults';
 import getEntryChunks from '../getEntryChunks';
 
-describe.skip('getEntryChunks', () => {
+const stats = {
+  chunks: [
+    defaultChunk({
+      id: 0,
+      names: ['chunk-zero'],
+    }),
+    defaultChunk({
+      id: 1,
+      names: ['chunk-one'],
+      parents: [0, 1],
+    }),
+    defaultChunk({
+      id: 2,
+      names: ['chunk-two'],
+    }),
+  ],
+};
+
+describe('getEntryChunks', () => {
   it('should list all the chunks', () => {
     const result = getEntryChunks(stats);
 
-    expect(result).toHaveLength(46);
+    expect(result).toHaveLength(3);
     expect(result).toMatchSnapshot();
   });
 });
