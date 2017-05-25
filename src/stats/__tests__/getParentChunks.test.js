b11937b9de542cde8a6eb17b5ebef27c8f6271c1
Ryan Albrecht <ryanalbrecht@pinterest.com>
2017-05-25 10:14:55 -0700

    Write some minimal tests for getParentChunks

--- src/stats/__tests__/getParentChunks.test.js
+++ src/stats/__tests__/getParentChunks.test.js
@@ -2,39 +2,71 @@
  * @flow
  */
 
-import stats from './__fixtures__/stats.json';
-
+import {defaultChunk} from '../../__test_helpers__/defaults';
 import getParentChunks from '../getParentChunks';
 
-describe.skip('getParentChunks', () => {
+const stats = {
+  chunks: [
+    defaultChunk({
+      id: 0,
+      names: ['chunk-zero'],
+    }),
+    defaultChunk({
+      id: 1,
+      names: ['chunk-one'],
+      parents: [0],
+    }),
+    defaultChunk({
+      id: 2,
+      names: ['chunk-two'],
+      parents: [0, 1],
+    }),
+    defaultChunk({
+      id: 3,
+      names: ['chunk-three'],
+      parents: [],
+    }),
+    defaultChunk({
+      id: 4,
+      names: ['chunk-four'],
+      parents: [0, 1, 3],
+    }),
+    defaultChunk({
+      id: 5,
+      names: ['chunk-five'],
+      parents: [4],
+    }),
+  ],
+};
+
+describe('getParentChunks', () => {
   it('should list some for a chunk in the middle', () => {
-    const result = getParentChunks(stats, 198);
+    const result = getParentChunks(stats, 1);
 
     expect(result).toEqual([
-      expect.objectContaining({id: 203}),
-      expect.objectContaining({id: 195}),
-      expect.objectContaining({id: 198}),
+      expect.objectContaining({id: 0}),
+      expect.objectContaining({id: 1}),
     ]);
   });
 
   it('should dive down to the bottom chunkIds', () => {
-    const result = getParentChunks(stats, 199);
+    const result = getParentChunks(stats, 5);
 
     expect(result).toEqual([
-      expect.objectContaining({id: 203}),
-      expect.objectContaining({id: 195}),
-      expect.objectContaining({id: 198}),
-      expect.objectContaining({id: 196}),
-      expect.objectContaining({id: 194}),
-      expect.objectContaining({id: 199}),
+      expect.objectContaining({id: 0}),
+      expect.objectContaining({id: 1}),
+      expect.objectContaining({id: 4}),
+      expect.objectContaining({id: 5}),
     ]);
   });
 
   it('should list just one for the top-level chunkId', () => {
-    const result = getParentChunks(stats, 203);
+    expect(getParentChunks(stats, 0)).toEqual([
+      expect.objectContaining({id: 0}),
+    ]);
 
-    expect(result).toEqual([
-      expect.objectContaining({id: 203}),
+    expect(getParentChunks(stats, 3)).toEqual([
+      expect.objectContaining({id: 3}),
     ]);
   });
 
