1502d4dd77fac46c4effe4eb4df210e0d3e99c25
Ryan Albrecht <ryanalbrecht@pinterest.com>
2017-05-25 10:16:19 -0700

    Cleanup App & Stats stories files.

--- src/components/__stories__/App.stories.js
+++ src/components/__stories__/App.stories.js
@@ -7,7 +7,6 @@ import { storiesOf, action } from '@kadira/storybook';
 
 import App from '../App';
 import FileInputRow from '../FileInputRow';
-// import stats from '../__fixtures__/stats-2017-03-13.json';
 import Stats from '../Stats';
 
 const stats = {
--- src/components/__stories__/Stats.stories.js
+++ src/components/__stories__/Stats.stories.js
@@ -5,7 +5,6 @@
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 
-// import stats from '../__fixtures__/stats-2017-03-13.json';
 import Stats from '../Stats';
 
 const stats = {
