4e689f7259baf00601a6db137b85985169fefcf7
Ryan Albrecht <ryan@ryanalbrecht.ca>
2017-05-29 16:39:43 -0700

    Push the latest Bonsai version to gh-pages with stats.json

--- deploy-example.sh
+++ deploy-example.sh
@@ -10,23 +10,26 @@ pages_folder=$(pwd)
 cd $(mktemp -d)
 git clone $pages_folder . --branch master
 
-yarn install --pure-lockfile && yarn run build
+yarn install --pure-lockfile
+
+NODE_ENV=production \
+    PUBLIC_URL=https://pinterest.github.io/bonsai/analyse/ \
+    REACT_APP_API_LIST_ENDPOINT=/bonsai/stats-index.json \
+    REACT_APP_EXTERNAL_URL_PREFIX=https://github.com/pinterest/bonsai/blob/master/ \
+    yarn run build
+
 rm -rf "$pages_folder/analyse"
 mv build "$pages_folder/analyse"
 
-NODE_ENV=development \
-    PUBLIC_URL=https://pinterest.github.io/bonsai/analyse/
-    REACT_APP_EXTERNAL_URL_PREFIX=https://github.com/pinterest/bonsai/blob/master/ \
-    ./node_modules/webpack/bin/webpack.js --json \
+./node_modules/webpack/bin/webpack.js --json \
     --config ./node_modules/react-scripts/config/webpack.config.dev.js > "$pages_folder/stats.json"
 
 rm -rf $(pwd)
 
 cd $pages_folder
 
-git config user.name $user_name
-git config user.email $user_email
-git commit -a -m 'Push the latest Bonsai version to gh-pages with stats.json'
+git add -A .
+git commit -m 'Push the latest Bonsai version to gh-pages with stats.json'
 git push origin
 
 if ! [[ $stashed == 'No local changes'* ]]; then
