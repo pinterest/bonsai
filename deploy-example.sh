05e88f8d32f7d40031aa0886121f35ed5b21a40c
Ryan Albrecht <ryan@ryanalbrecht.ca>
2017-05-29 16:33:33 -0700

    Bake in the correct PUBLIC_URL when deploying to github pages.
    Also, lets enable auto-commit now that we've got conidance in this script.

--- deploy-example.sh
+++ deploy-example.sh
@@ -6,8 +6,6 @@ stashed=$(git stash -u)
 echo $stashed
 
 pages_folder=$(pwd)
-user_name=$(git config user.name)
-user_email=$(git config user.email)
 
 cd $(mktemp -d)
 git clone $pages_folder . --branch master
@@ -17,18 +15,20 @@ rm -rf "$pages_folder/analyse"
 mv build "$pages_folder/analyse"
 
 NODE_ENV=development \
-REACT_APP_EXTERNAL_URL_PREFIX=https://github.com/pinterest/bonsai/blob/master/ \
+    PUBLIC_URL=https://pinterest.github.io/bonsai/analyse/
+    REACT_APP_EXTERNAL_URL_PREFIX=https://github.com/pinterest/bonsai/blob/master/ \
     ./node_modules/webpack/bin/webpack.js --json \
     --config ./node_modules/react-scripts/config/webpack.config.dev.js > "$pages_folder/stats.json"
 
+rm -rf $(pwd)
+
+cd $pages_folder
+
 git config user.name $user_name
 git config user.email $user_email
-git add analyse/ stats.json
-git commit -m 'Push the latest Bonsai version to gh-pages with stats.json'
-# git push origin
+git commit -a -m 'Push the latest Bonsai version to gh-pages with stats.json'
+git push origin
 
 if ! [[ $stashed == 'No local changes'* ]]; then
     git stash pop
 fi
-
-rm -rf $(pwd)
