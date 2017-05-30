13a7ba5338f6c0e78c33119b820e5d099a93fb1f
Ryan Albrecht <ryan@ryanalbrecht.ca>
2017-05-29 20:29:58 -0700

    Rename webpage to Bonsai

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export default function Navbar() {
       <div className="container-fluid">
         <div className="navbar-header">
           <a className="navbar-brand" href="/">
-            Webpack Dependency Size
+            Bonsai - Trim your dependency trees
           </a>
         </div>
         <ul className="nav navbar-nav navbar-right">
