bea11a51dae8dc41c3617355e85ab9d300c6978f
Ryan Albrecht <ryanalbrecht@pinterest.com>
2017-06-01 19:52:51 -0700

    Rename stats to example.json for clarity

--- deploy-example.sh
+++ deploy-example.sh
@@ -16,7 +16,7 @@ yarn install --pure-lockfile
 
 NODE_ENV=production \
     PUBLIC_URL=https://pinterest.github.io/bonsai/analyze/ \
-    REACT_APP_API_LIST_ENDPOINT=/bonsai/stats-index.json \
+    REACT_APP_API_LIST_ENDPOINT=/bonsai/example-index.json \
     REACT_APP_EXTERNAL_URL_PREFIX= \
     yarn run build
 
@@ -25,14 +25,14 @@ mv build "$pages_folder/analyze"
 
 NODE_ENV=production \
     ./node_modules/webpack/bin/webpack.js --json \
-    --config ./node_modules/react-scripts/config/webpack.config.prod.js > "$pages_folder/stats.json"
+    --config ./node_modules/react-scripts/config/webpack.config.prod.js > "$pages_folder/example.json"
 
 rm -rf $(pwd)
 
 cd $pages_folder
 
 git add -A .
-git commit -m 'Push the latest Bonsai version to gh-pages with stats.json'
+git commit -m 'Push the latest Bonsai version to gh-pages with example.json'
 git push origin
 
 if ! [[ $stashed == 'No local changes'* ]]; then
