70b90e62b9a51a43220eb063f4572aabf0c03f2e
Ryan Albrecht <ryan@ryanalbrecht.ca>
2017-06-05 22:34:49 -0700

    Improve some flow coverage.

--- src/debounce.js
+++ src/debounce.js
@@ -2,13 +2,13 @@
  * @flow
  */
 
-export default function debounce(fn: Function, delay: number = 250) {
+export default function debounce(
+  fn: () => void,
+  delay: number = 250,
+): () => void {
   let timer = null;
   return function() {
-    const args = arguments;
     clearTimeout(timer);
-    timer = setTimeout(() => {
-      fn.apply(this, args);
-    }, delay);
+    timer = setTimeout(fn, delay);
   };
 }
