ad848822508d1c1035a9b4af83ee25102c02e26e
Ryan Albrecht <ryan@ryanalbrecht.ca>
2017-06-17 14:54:59 -0700

    Cleanup extra *.stories files.

--- src/components/stories.js
+++ src/components/stories.js
@@ -8,10 +8,9 @@ import { storiesOf, action } from '@storybook/react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../index.css';
 
-// import './__stories__/App.stories';
+import './__stories__/App.stories';
 import './__stories__/FileInputRow.stories';
 import './__stories__/SortLabel.stories';
-// import './__stories__/Stats.stories';
 import './__stories__/Unit.stories';
 
 import './Bootstrap/__stories__/Button.stories';
