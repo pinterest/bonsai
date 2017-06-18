c9691a10ed3aeb06c2eb2ec208516b8a52de8b32
Ryan Albrecht <ryan@ryanalbrecht.ca>
2017-06-17 21:53:48 -0700

    Add an app manifest & service worker
    Following the steps from cra:
    https://github.com/facebookincubator/create-react-app/blob/master/CHANGELOG.md#100-may-18-2017
    https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md#making-a-progressive-web-app

--- src/index.js
+++ src/index.js
@@ -7,11 +7,10 @@ import './shims';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
