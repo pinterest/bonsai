cc797542356c278d067fc02f48bb8e3a8d2bd640
Ryan Albrecht <ryanalbrecht@pinterest.com>
2017-07-11 16:40:16 -0700

    Also let the gh-pages script run in dev, in case you want to do that.

--- build-gh-pages.sh
+++ build-gh-pages.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-if [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ -z "$TRAVIS_BRANCH" ] || [ "$TRAVIS_BRANCH" == "master" ]; then
     NODE_ENV=production \
         PUBLIC_URL=https://pinterest.github.io/bonsai/analyze/ \
         REACT_APP_API_LIST_ENDPOINT=/bonsai/example-index.json \
         REACT_APP_EXTERNAL_URL_PREFIX= \
-        yarn run build && \\
+        yarn run build && \
         mv build docs/analyze
     NODE_ENV=production \
         ./node_modules/webpack/bin/webpack.js --json \
