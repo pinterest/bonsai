b29090e7b503bd73f6c955b93960dfca0cea6e32
Ryan Albrecht <ryan@ryanalbrecht.ca>
2017-07-25 20:59:53 -0700

    Wrap all stories with the redux store.

--- src/components/__stories__/App.stories.js
+++ src/components/__stories__/App.stories.js
@@ -9,18 +9,15 @@ import handleAction from '../../reducer';
 import React from 'react';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import { storiesOf } from '@storybook/react';
+import { storiesOf, addDecorator } from '@storybook/react';
 
 const stats = {
   chunks: [],
   modules: [],
 };
 
-const store = createStore(handleAction);
-
 storiesOf('App', module)
   .add('Default View', () => (
-    <Provider store={store}>
     <App
       dataPaths={[]}
       filename={null}
@@ -32,20 +29,17 @@ storiesOf('App', module)
       onLoaded={function(filename, stats) {}}
       onDroppedFile={function(filename, fileText) {}}
     />
-    </Provider>
   ))
   .add('FileInput stacked on Stats', () => (
-    <Provider store={store}>
-      <App
-        dataPaths={[]}
-        filename={'stats.json'}
-        loading={false}
-        json={stats}
-        onInitDataPaths={(paths) => undefined}
-        onPickedFile={() => undefined}
-        onLoadingFailed={() => undefined}
-        onLoaded={function(filename, stats) {}}
-        onDroppedFile={function(filename, fileText) {}}
-      />
-    </Provider>
+    <App
+      dataPaths={[]}
+      filename={'stats.json'}
+      loading={false}
+      json={stats}
+      onInitDataPaths={(paths) => undefined}
+      onPickedFile={() => undefined}
+      onLoadingFailed={() => undefined}
+      onLoaded={function(filename, stats) {}}
+      onDroppedFile={function(filename, fileText) {}}
+    />
   ));
