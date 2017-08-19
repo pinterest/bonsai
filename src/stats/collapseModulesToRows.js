d6e26ffeeb1a0067d49744efc41646dc41a3c006
Ryan Albrecht <ryan@ryanalbrecht.ca>
2017-08-19 12:19:47 -0700

    Group modules that only have one parent under that parent to reduce the number of noisy rows in the output.
    By focusing on just the entry-points to these little chains we can skip a bunch of noise.

--- src/stats/collapseModulesToRows.js
+++ src/stats/collapseModulesToRows.js
@@ -8,11 +8,41 @@ import type {
 } from '../types/Stats';
 import type { ExtendedModulesById } from './getModulesById';
 
+import getModulesById from './getModulesById';
+import flatten from '../flatten';
+
+function collectSingleChildrenOf(
+  modulesById: ExtendedModulesById,
+  module: ExtendedModule,
+): Array<ExtendedModule> {
+  return flatten(
+    module.requirements.map((requiredModule) => {
+      const eReqModule = modulesById[requiredModule.id];
+
+      if (eReqModule && eReqModule.requiredByCount === 1) {
+        return [
+          eReqModule,
+          ...collectSingleChildrenOf(modulesById, eReqModule)
+        ];
+      } else {
+        return [];
+      }
+    })
+  );
+}
+
 export default function(
   modules: Array<ExtendedModule>,
 ): Array<RowRepresentation> {
-  return modules.map((eModule) => ({
-    displayModule: eModule,
-    records: [eModule],
-  }));
+
+  const modulesById = getModulesById(modules);
+
+  return modules.filter((eModule) => {
+    return eModule.requiredByCount !== 1;
+  }).map((eModule) => {
+    return {
+      displayModule: eModule,
+      records: [eModule, ...collectSingleChildrenOf(modulesById, eModule)],
+    };
+  });
 }
