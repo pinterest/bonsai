a989e731cdbf0543d747bc035f2a76cbe345db82
Ryan Albrecht <ryan@ryanalbrecht.ca>
2017-08-26 23:52:08 -0700

    Do all module filtering/sorting/grouping inside ModuleTable not the Container.

--- src/components/stats/ModuleTable.js
+++ src/components/stats/ModuleTable.js
@@ -10,19 +10,15 @@ import type {
   SortableFields,
   SortProps,
 } from '../../stats/sortModules';
-import type {
-  ModuleID,
-  RowRepresentation,
-} from '../../types/Stats';
+import type { ModuleID } from '../../types/Stats';
 
+import collapseModulesToRows from '../../stats/collapseModulesToRows';
+import filterModules from '../../stats/filterModules';
 import ModuleTableBody from './ModuleTableBody';
 import ModuleTableHead from './ModuleTableHead';
 import React, { Component } from 'react';
 import scrollToAndFocus from '../../scrollToAndFocus';
-
-export type OwnProps = {
-  rows: Array<RowRepresentation>,
-};
+import sortModules from '../../stats/sortModules';
 
 export type StateProps = {
   filters: FilterProps,
@@ -39,7 +35,7 @@ export type DispatchProps = {
   onCollapseRecords: (moduleID: ModuleID) => void,
 };
 
-export type Props = OwnProps & StateProps & DispatchProps;
+export type Props = StateProps & DispatchProps;
 
 export default class ModuleTable extends Component {
   componentDidUpdate() {
@@ -50,6 +46,20 @@ export default class ModuleTable extends Component {
 
   render() {
     const props = this.props;
+
+    const rows = collapseModulesToRows(
+      sortModules(
+        filterModules(
+          props.extendedModules,
+          props.filters,
+        ),
+        props.sort,
+      ),
+    );
+    rows.forEach((row) => {
+      row.records = sortModules(row.records, props.sort);
+    });
+
     return (
       <table className="table table-hover" cellPadding="0" cellSpacing="0">
         <ModuleTableHead
@@ -59,7 +69,7 @@ export default class ModuleTable extends Component {
           onFilter={props.onFilterChanged}
         />
         <ModuleTableBody
-          rows={props.rows}
+          rows={rows}
           expandedRecords={props.expandedRecords}
           onRemoveModule={props.onRemoveModule}
           onExpandRecords={props.onExpandRecords}
--- src/components/stats/ModuleTableContainer.js
+++ src/components/stats/ModuleTableContainer.js
@@ -3,14 +3,11 @@
  */
 
 import type { Dispatch, State } from '../../reducer';
-import type { DispatchProps, StateProps, Props } from './ModuleTable';
+import type { DispatchProps, StateProps } from './ModuleTable';
 import type {
   ExtendedModule,
 } from '../../types/Stats';
 
-import collapseModulesToRows from '../../stats/collapseModulesToRows';
-import filterModules from '../../stats/filterModules';
-import sortModules from '../../stats/sortModules';
 import ModuleTable from './ModuleTable';
 import {connect} from 'react-redux';
 import {
@@ -44,28 +41,7 @@ const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => {
   };
 };
 
-const mergeProps = (
-  stateProps: StateProps,
-  dispatchProps: DispatchProps,
-  ownProps: OwnProps,
-): Props => {
-  return {
-    ...stateProps,
-    ...dispatchProps,
-    rows: collapseModulesToRows(
-      sortModules(
-        filterModules(
-          ownProps.extendedModules,
-          stateProps.filters,
-        ),
-        stateProps.sort,
-      ),
-    ),
-  };
-};
-
 export default connect(
   mapStateToProps,
   mapDispatchToProps,
-  mergeProps,
 )(ModuleTable);
