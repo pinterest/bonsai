eaf0530bc1c5567ea68f2f6e2bfd4e60098e5c80
Ryan Albrecht <ryan@ryanalbrecht.ca>
2017-09-10 16:05:26 -0700

    do dynamic storyshot loading

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,6 +2,9 @@ import { configure } from '@storybook/react';
 
 function loadStories() {
   require('../src/components/stories');
+
+  const req = require.context('../src/components', true, /\.stories\.js$/);
+  req.keys().forEach(req);
 }
 
 configure(loadStories, module);
--- src/components/stories.js
+++ src/components/stories.js
@@ -4,14 +4,3 @@
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../index.css';
-
-import './__stories__/App.stories';
-import './__stories__/FileInputRow.stories';
-import './__stories__/SortLabel.stories';
-import './__stories__/Unit.stories';
-
-import './Bootstrap/__stories__/Button.stories';
-import './Bootstrap/__stories__/Dropdown.stories';
-
-import './stats/__stories__/ExternalModuleLink.stories.js';
-import './stats/__stories__/formatModuleName.stories';
