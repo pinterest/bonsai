12abe623af93287ee9ad20ef870711216bf2c46b
Ryan Albrecht <ryan@ryanalbrecht.ca>
2021-02-22 17:54:45 +0000

    Shim console.timeStamp() to no-op

--- src/shims.js
+++ src/shims.js
@@ -6,6 +6,8 @@ import values from 'object.values';
 import includes from 'array-includes';
 import 'raf/polyfill';
 
+console.timeStamp = function() {};
+
 if (!Object.values) {
   values.shim();
 }
